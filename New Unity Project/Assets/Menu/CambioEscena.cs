7fd19c96ab1bad4e55c407a20e47ec204134581d
CeciliaMJ19 <19460699@colima.tecnm.mx>
2021-10-15 14:59:39 -0500

    Menú Categorías

---
+++ New Unity Project/Assets/Menu/CambioEscena.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioEscena : MonoBehaviour
+{
+    public int escena;
+
+    public void CambiarEscena()
+    {
+            SceneManager.LoadScene(escena);
+    }
+}
