b8d5a023934f49557c081b6d6652a29b44c9ab52
CarlosGuedea <guedea.fis@gmail.com>
2021-10-23 12:28:40 -0500

    Detallado Escenario 1
    Se agregan las cartas automaticamente, falta ajustarlas y agregar imagenes

---
+++ New Unity Project/Assets/Scenes/Carta.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carta : MonoBehaviour
+{
+    public Material ColorCarta;
+    public int NumCarta=0; 
+
+    
+    void OnMouseDown()
+    {
+       print (NumCarta.ToString ());
+    }
+
+    
+    public void PonerColor(Material color_)
+    {
+        GetComponent <SpriteRenderer> ().material = color_;
+        ColorCarta = color_;
+    }
+}
---
+++ New Unity Project/Assets/Scenes/CrearCartas.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrearCartas : MonoBehaviour
+{
+    public GameObject CartaPrefab;
+    public int Ancho;
+    public int Alto;
+    public Material Blanco;
+    public Material Negro;
+
+    void Start(){
+        Crear ();
+    }
+
+    public void Crear()
+    {
+        int count = 0;
+        for(int i=0;i<Ancho;i++){
+            for(int x=0;x<Alto;x++){
+                GameObject CartaTemp=Instantiate(CartaPrefab, new Vector2(x,i),Quaternion.identity);
+                CartaTemp.GetComponent<Carta>().NumCarta=count;
+                count++;
+            }
+        }
+    }
+}
